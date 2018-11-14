fc99b4f89904e6171229b55d9112cdceb8a2bea2
Invariant Telecom <Invariant@Ss-iMac.local>
2018-11-14 16:31:42 +0600

    Added Setting Intent

--- app/src/main/java/io/github/gafurcseku/internetconnectivitychecker/MainActivity.java
+++ app/src/main/java/io/github/gafurcseku/internetconnectivitychecker/MainActivity.java
@@ -3,6 +3,7 @@ package io.github.gafurcseku.internetconnectivitychecker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -10,8 +11,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         if(NetworkUtil.isConnected(this)){
             Toast.makeText(this,"Connected", Toast.LENGTH_LONG).show();
+        }else{
+            NetworkUtil.openSettings(this);
         }
     }
 }
