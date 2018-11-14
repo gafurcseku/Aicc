15c3afea90b5de2a5b2e9c73503eba242236e3d8
Invariant Telecom <Invariant@Ss-iMac.local>
2018-11-14 11:51:07 +0600

    Change Process

---
+++ app/src/main/java/io/github/gafurcseku/internetconnectivitychecker/MainActivity.java
@@ -0,0 +1,17 @@
+package io.github.gafurcseku.internetconnectivitychecker;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(NetworkUtil.isConnected(this)){
+            Toast.makeText(this,"Connected", Toast.LENGTH_LONG).show();
+        }
+    }
+}
